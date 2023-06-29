81a3b268c343c2afd257b82b32eb960df3ad90a7
Focused Meanderer <84609482+FocusedMeanderer@users.noreply.github.com>
2023-06-29 16:07:18 +0800

    Add pen color options and a way to clear canvas
    Let user select from predefined colors or a random color each tile

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 const container = document.querySelector('div.container');
 const go = document.querySelector('button.go');
 let   newDim = 16;
+const GRID_SZ = 800;
+const PENS = {
+  black:  { r: 0,   g: 0,   b: 0   },
+  red:    { r: 255, g: 0,   b: 0   },
+  orange: { r: 255, g: 165, b: 0   },
+  yellow: { r: 255, g: 255, b: 0   },
+  green:  { r: 0,   g: 255, b: 0   },
+  blue:   { r: 0,   g: 0,   b: 255 },
+  indigo: { r: 750, g: 0,   b: 130 },
+  violet: { r: 238, g: 130, b: 238 },
+  kaleidoscope: { r: -1, g: -1, b: -1 }
+}
+let pen = PENS['black'];
+
+const clear = document.querySelector('button.clear');
+const colors = document.querySelector('div.colors');
 
 function createGrid(dim = 16) {
   for (let i = 0; i < dim*dim; i++) {
     const aDiv = document.createElement('div');
-    aDiv.style.width = `${Math.floor(100/dim * 100) / 100}%`;
-    aDiv.style.height = `${Math.floor(960/dim * 100) / 100}px`;
+    aDiv.style.width = `${Math.floor(GRID_SZ/dim * 100) / 100}px`;
+    aDiv.style.height = `${Math.floor(GRID_SZ/dim * 100) / 100}px`;
     aDiv.style.border = '1px solid black';
     aDiv.style.border = '1px dotted lightgrey';
     aDiv.style.textAlign = 'center';
@@ -49,10 +65,42 @@ function validateInput() {
 createGrid();
 
 go.addEventListener('click', recreateGrid);
+clear.addEventListener('click', clearCanvas);
+colors.addEventListener('click', setPenColor);
+
+function setPenColor(e) {
+  if (e.target.classList.contains('pen')) {
+    const penColor = document.querySelector('div.palette');
+
+    if (e.target.id === 'kaleidoscope') {
+      penColor.style.backgroundImage = 'linear-gradient(to right, black, indigo)';
+    }
+    else {
+      penColor.style.backgroundImage = '';
+      penColor.style.backgroundColor = e.target.id;
+    }
+    
+    pen = PENS[e.target.id];
+  }
+}
+
+function clearCanvas(e) {
+  const divs = document.querySelectorAll('div.grid-item');
+
+  divs.forEach(aDiv => {
+    aDiv.style.backgroundColor = 'white';
+    aDiv.dataset.intensity = 0.1;
+  });
+}
 
 function fillItem(e) {
   if (e.target.classList.contains('grid-item')) {
-    e.target.style.backgroundColor = `rgba(0,0,0,${e.target.dataset.intensity})`;
+    if (pen['r'] === -1 && pen['g'] === -1 && pen['b'] === -1) {
+      e.target.style.backgroundColor = `rgba(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${e.target.dataset.intensity})`;
+    }
+    else {
+      e.target.style.backgroundColor = `rgba(${pen['r']},${pen['g']},${pen['b']},${e.target.dataset.intensity})`;
+    }
     if ((e.target.dataset.intensity * 10) < 10) {
       e.target.dataset.intensity = ((e.target.dataset.intensity * 10) + 1)/10;
     }
