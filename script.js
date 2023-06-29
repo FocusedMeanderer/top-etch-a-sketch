3c0ba2e12406e4553dc420b90bf3ec78318e8cee
Focused Meanderer <84609482+FocusedMeanderer@users.noreply.github.com>
2023-06-29 11:56:02 +0800

    Add initial layout
    - Add function to layout grid (default 16x16) via Javascript
    - Add function to remove grid via Javascript
    - Add an input mechanism in the Html file

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 const container = document.querySelector('div.container');
+
+function createGrid(dim = 16) {
+  for (let i = 0; i < dim*dim; i++) {
+    const aDiv = document.createElement('div');
+    aDiv.style.width = `${Math.floor(100/16 * 100) / 100}%`;
+    aDiv.style.height = '40px';
+    aDiv.style.border = '1px solid black';
+    aDiv.textContent = +(i+1);
+    aDiv.style.border = '1';
+    aDiv.style.textAlign = 'center';
+    aDiv.classList.add('gridItem');
+
+    container.appendChild(aDiv);
+  }
+}
+
+function removeGrid() {
+  const divs = document.querySelectorAll('div.gridItem');
+
+  console.log(divs);
+
+  divs.forEach(aDiv => {
+    container.removeChild(aDiv);
+  });
+}
+
+createGrid();
